b20b3a6b4a1f042a645fb220d9642bccbc102211
romanhrynevych <82209198+RomanHrynevych@users.noreply.github.com>
2021-08-04 14:38:35 +0300

    Added loging and interests

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: 'Steven Thomas Williams',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: "Sarah Smith",
+  owner: 'Sarah Smith',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -36,30 +36,30 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
-const labelBalance = document.querySelector(".balance__value");
-const labelSumIn = document.querySelector(".summary__value--in");
-const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-const labelTimer = document.querySelector(".timer");
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector(".login__btn");
-const btnTransfer = document.querySelector(".form__btn--transfer");
-const btnLoan = document.querySelector(".form__btn--loan");
-const btnClose = document.querySelector(".form__btn--close");
-const btnSort = document.querySelector(".btn--sort");
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector(".login__input--user");
-const inputLoginPin = document.querySelector(".login__input--pin");
-const inputTransferTo = document.querySelector(".form__input--to");
-const inputTransferAmount = document.querySelector(".form__input--amount");
-const inputLoanAmount = document.querySelector(".form__input--loan-amount");
-const inputCloseUsername = document.querySelector(".form__input--user");
-const inputClosePin = document.querySelector(".form__input--pin");
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
@@ -70,9 +70,9 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
 ]);
 
 // Calc and Display Balance
@@ -94,17 +94,19 @@ const displayMovements = function (movements) {
         <div class="movements__value">${mov}€</div>
       </div>
     `;
-    containerMovements.insertAdjacentHTML("afterbegin", html);
+    containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
 // IN / OUT / INTEREST
-const inOutInterestDisplay = function (movements) {
-  const deposits = movements.reduce((acc, mov) => (mov > 0 ? acc + mov : acc));
-  const withdrawals = movements.reduce((acc, mov) =>
+const calcSummary = function (account) {
+  const deposits = account.movements.reduce((acc, mov) =>
+    mov > 0 ? acc + mov : acc
+  );
+  const withdrawals = account.movements.reduce((acc, mov) =>
     mov < 0 ? acc + mov : acc
   );
-  const interest = (deposits * 1.2) / 100;
+  const interest = (deposits * account.interestRate) / 100;
   labelSumIn.innerHTML = `${deposits}€`;
   labelSumOut.innerHTML = `${withdrawals}€`;
   labelSumInterest.innerHTML = `${interest}€`;
@@ -122,15 +124,42 @@ const createUsernames = function (accs) {
     acc.username = acc.owner
       .toLowerCase()
       .split(` `)
-      .map((letter) => letter[0])
+      .map(letter => letter[0])
       .join(``);
   });
 };
-
-displayMovements(movements);
-calcDisplayBalance(movements);
 createUsernames(accounts);
-inOutInterestDisplay(movements);
+
+let currentUser;
+
+// Login form event listener
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  currentUser = accounts.find(acc => acc.username === inputLoginUsername.value);
+  if (currentUser?.pin === Number(inputLoginPin.value)) {
+    // Clear form fields
+    inputLoginPin.value = inputLoginUsername.value = '';
+    inputLoginPin.blur();
+    inputLoginUsername.blur();
+    // Display "Hello" message
+    labelWelcome.textContent = `Welcome back, ${
+      currentUser.owner.split(' ')[0]
+    }`;
+    // Calc Balance
+    calcDisplayBalance(currentUser.movements);
+    // Calc summary
+    calcSummary(currentUser);
+    // Display movements
+    displayMovements(currentUser.movements);
+    //Display UI
+    containerApp.style.opacity = 1;
+  } else {
+    // If Login or password is incorrect
+    alert(`Login or password is incorrect`);
+    return;
+  }
+});
+
 /////////////////////////////////////////////////
 
 // const dogsJulia = [3, 5, 2, 12, 7];
