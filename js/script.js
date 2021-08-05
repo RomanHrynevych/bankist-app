8870dc9a3b7e24f29b6463b4c96c5ed3d0292196
RomanHrynevych <hrynevych.romann@gmail.com>
2021-08-06 00:00:59 +0300

    Finished section

--- js/script.js
+++ js/script.js
@@ -247,10 +247,10 @@ btnSort.addEventListener('click', function (e) {
   sortBool = sortBool === 3 ? 0 : sortBool;
   if (sortBool === 0) {
     displayMovements(currentUser.movements);
-    btnSort.textContent = `SORT`;
+    btnSort.textContent = `↑ SORT`;
   } else if (sortBool === 1) {
     displayMovements([...currentUser.movements].sort((a, b) => a - b));
-    btnSort.textContent = '&downarrow; SORT';
+    btnSort.textContent = '↓ SORT';
   } else {
     displayMovements([...currentUser.movements].sort((a, b) => b - a));
     btnSort.textContent = 'UNSORT';
@@ -292,3 +292,111 @@ btnSort.addEventListener('click', function (e) {
 //     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 // };
 // console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+
+// const arrDiceRolls = Array.from(
+//   { length: 100 },
+//   () => Math.floor(Math.random() * 6) + 1
+// );
+// console.log(Math.max(...arrDiceRolls), Math.min(...arrDiceRolls), arrDiceRolls);
+
+// // How many deposits from all accounts
+// const bankDepositsSum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(el => el > 0)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(bankDepositsSum);
+
+// // How many deposits more equal than 1000
+// const bankDepositsMoreOneThou = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(el => el >= 1000).length;
+// console.log(bankDepositsMoreOneThou);
+
+// // How many deposits and withdrawals
+// const sumDepositsWithdrawals = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+// console.log(sumDepositsWithdrawals);
+
+// Coding challenge #4
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+const eatNormal = function (mustEat, eating) {
+  if (eating <= mustEat * 1.1 && eating >= mustEat * 0.9) {
+    return true;
+  }
+  return false;
+};
+
+// 1 task
+dogs.forEach(function (dog) {
+  dog.recFood = Number((dog.weight ** 0.75 * 28).toFixed());
+});
+console.log(dogs);
+
+// 2 task
+const SarahDog = dogs.filter(el => el.owners.includes('Sarah'))[0];
+console.log(SarahDog);
+console.log(eatNormal(SarahDog.recFood, SarahDog.curFood));
+
+// 3 task
+const divideDogs = dogs.reduce(
+  (arrays, cur) => {
+    if (cur.curFood > cur.recFood) {
+      arrays.ownersEatTooMuch.push(...cur.owners);
+    } else if (cur.curFood < cur.recFood) {
+      arrays.ownersEatTooLittle.push(...cur.owners);
+    }
+    return arrays;
+  },
+  {
+    ownersEatTooMuch: [],
+    ownersEatTooLittle: [],
+  }
+);
+console.log(divideDogs);
+
+// 4 task
+console.log(
+  `${divideDogs.ownersEatTooMuch.join(' and ')}'s dogs eat too much!`
+);
+console.log(
+  `${divideDogs.ownersEatTooLittle.join(' and ')}'s dogs eat too little!`
+);
+
+// 5 task
+console.log(
+  `Task 5`,
+  dogs.some(dog => dog.curFood === dog.recFood)
+);
+
+// 6 task
+console.log(
+  `Task 6`,
+  dogs.some(dog => eatNormal(dog.recFood, dog.curFood))
+);
+
+// 7 task
+const dogsEatingNormal = [];
+dogs.forEach(dog => {
+  if (eatNormal(dog.recFood, dog.curFood)) {
+    dogsEatingNormal.push(dog);
+  }
+});
+console.log(`Task 7`, ...dogsEatingNormal);
+
+// 8 Task
+const dogsCopy = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(...dogs);
+console.log(...dogsCopy);
